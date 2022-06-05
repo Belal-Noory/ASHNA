df43cc990d76f17f4dc1089aa643075e639c12bb
BELAL NOORY <belalnoory2@gmail.com>
2022-06-05 09:37:17 +0430

    revenue list done

--- app/Models/Revenue.php
+++ app/Models/Revenue.php
@@ -18,12 +18,12 @@ class Revenue
         return $result;
     }
 
-    public function getReceiptLeadger($companyID)
+    public function getRevenueLeadger($companyID)
     {
-        $query = "SELECT * FROM general_leadger INNER JOIN account_money ON general_leadger.leadger_id = account_money.leadger_ID 
-                INNER JOIN company_currency ON general_leadger.currency_id = company_currency.company_currency_id
+        $query = "SELECT * FROM general_leadger LEFT JOIN account_money ON general_leadger.leadger_id = account_money.leadger_ID 
+                LEFT JOIN company_currency ON general_leadger.currency_id = company_currency.company_currency_id
                 WHERE account_money.ammount_type = ? AND general_leadger.company_id = ? AND general_leadger.op_type = ?";
-        $result = $this->conn->Query($query, ["Crediet",$companyID,"Receipt"]);
+        $result = $this->conn->Query($query, ["debet", $companyID, "Revenue"]);
         return $result;
     }
 
@@ -36,9 +36,10 @@ class Revenue
     }
 
     // Get Revenue Accounts
-     public function getRevenueAccounts($company_id){
+    public function getRevenueAccounts($company_id)
+    {
         $query = "SELECT * FROM chartofaccount WHERE account_kind = ? AND company_id = ?";
-        $result = $this->conn->Query($query, ['Revenue',$company_id]);
+        $result = $this->conn->Query($query, ['Revenue', $company_id]);
         return $result;
     }
 
@@ -53,7 +54,8 @@ class Revenue
     }
 
     // Get Revenue all list from account_catagory
-    public function getRevenueListWithChildred(){
+    public function getRevenueListWithChildred()
+    {
         $query = "SELECT account_catagory_id FROM account_catagory WHERE catagory = ? ";
         $result = $this->conn->Query($query, ['revenue']);
         $results = $result->fetch(PDO::FETCH_OBJ);
@@ -69,13 +71,11 @@ class Revenue
         $data = array();
         $res = $result->fetchAll(PDO::FETCH_OBJ);
         foreach ($res as $re) {
-            if($re->parentID != 0)
-            {
+            if ($re->parentID != 0) {
                 $child = $this->getSubCategories($re->account_catagory_id);
-                array_push($data,["parent"=>array("name"=>$re->catagory,"id"=>$re->account_catagory_id),"child"=>$child]);
-            }
-            else{
-                array_push($data,["parent"=>array("name"=>$re->catagory,"id"=>$re->account_catagory_id),"child"=>[]]);
+                array_push($data, ["parent" => array("name" => $re->catagory, "id" => $re->account_catagory_id), "child" => $child]);
+            } else {
+                array_push($data, ["parent" => array("name" => $re->catagory, "id" => $re->account_catagory_id), "child" => []]);
             }
         }
         return $data;
