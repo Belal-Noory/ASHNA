d33f5b63c392cc71a61ee6613167a426a7c3d6b0
Belal Noory <belalnoory2@gmail.com>
2022-06-05 08:27:17 -0700

    payment adding is done

---
+++ app/Models/Payments.php
@@ -0,0 +1,37 @@
+<?php
+
+class Payments
+{
+    // Database class
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new Connection();
+    }
+
+    public function addPaymentLeadger($params)
+    {
+        $query = "INSERT INTO general_leadger(recievable_id,payable_id,currency_id,remarks,company_financial_term_id,reg_date,currency_rate,approved,createby,updatedby,op_type,company_id) 
+        VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        $result = $this->conn->Query($query, $params, true);
+        return $result;
+    }
+
+    public function getPaymentLeadger($companyID)
+    {
+        $query = "SELECT * FROM general_leadger INNER JOIN account_money ON general_leadger.leadger_id = account_money.leadger_ID 
+                INNER JOIN company_currency ON general_leadger.currency_id = company_currency.company_currency_id
+                WHERE account_money.ammount_type = ? AND general_leadger.company_id = ? AND general_leadger.op_type = ?";
+        $result = $this->conn->Query($query, ["Crediet",$companyID,"Receipt"]);
+        return $result;
+    }
+
+    public function getPaymentAccount($leadger_id)
+    {
+        $query = "SELECT * FROM account_money INNER JOIN chartofaccount ON account_money.account_id = chartofaccount.chartofaccount_id 
+                 WHERE account_money.leadger_ID = ? ";
+        $result = $this->conn->Query($query, [$leadger_id]);
+        return $result;
+    }
+}
