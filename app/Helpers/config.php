7ec4fc1489996c19a0c5e5937103f6b3efe035ce
BELAL NOORY <belalnoory2@gmail.com>
2022-06-22 14:08:50 +0430

    online credential added

--- app/Helpers/config.php
+++ app/Helpers/config.php
@@ -1,9 +1,14 @@
 <?php
 // DB Params
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'ashna');
+
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'ashna');
+define('DB_USER', 'u645214731_tweV4');
+define('DB_PASS', 't7wwIpQ?');
+define('DB_NAME', 'u645214731_pKSzv');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
