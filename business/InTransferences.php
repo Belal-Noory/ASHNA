cdbb7626b9a947fc13a8c4f98a07937bdb7b7681
Belal Noory <belalnoory2@gmail.com>
2022-07-22 13:49:44 -0700

    newupd

--- business/InTransferences.php
+++ business/InTransferences.php
@@ -459,7 +459,7 @@ include("./master/footer.php");
 
         });
 
-        $(document).on("click", "#addapproval", function() {
+        $(document).on("click", ".btnapprove", function() {
             e.preventDefault();
             ths = $(this);
             transfer_id = $(ths).attr("data-href");
