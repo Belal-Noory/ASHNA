add64bca51da6926780183b9068e97c34ae0cc4c
Belal Noory <belalnoory2@gmail.com>
2022-07-23 10:50:33 -0700

    updated

--- app/Models/Banks.php
+++ app/Models/Banks.php
@@ -81,8 +81,8 @@ class Banks
 
     public function getAccount($companyID, $type)
     {
-        $query = "SELECT * FROM chartofaccount WHERE company_id = ? AND account_catagory = ?";
-        $result = $this->conn->Query($query, [$companyID, $type]);
+        $query = "SELECT * FROM chartofaccount WHERE company_id = ? AND account_catagory = ? AND useradded = ?";
+        $result = $this->conn->Query($query, [$companyID, $type, 1]);
         return $result;
     }
 
