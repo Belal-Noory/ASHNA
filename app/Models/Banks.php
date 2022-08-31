3d6e45414a14f772642e17dc59448543c6dd87bb
Belal Noory <belalnoory2@gmail.com>
2022-08-31 04:50:08 -0700

    updated

--- app/Models/Banks.php
+++ app/Models/Banks.php
@@ -128,9 +128,9 @@ class Banks
 
     public function addOpeningBalanceLeadger($params)
     {
-        $query = "INSERT INTO `general_leadger`(`recievable_id`, `payable_id`, `currency_id`, `remarks`, `company_financial_term_id`, `reg_date`, `currency_rate`, `approved`, `createby`, `updatedby`, `op_type`, `cleared`, `company_id`, `rcode`, `deleted`)
-         VALUES ('[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]','[?]')";
-        $result = $this->conn->Query($query, $params, true);
+        $query = "INSERT INTO general_leadger(recievable_id,currency_id,remarks,company_financial_term_id,reg_date,approved,createby,updatedby,op_type,company_id) 
+        VALUES(?,?,?,?,?,?,?,?,?,?)";
+        $result = $this->conn->Query($query, [$params], true);
         return $result;
     }
 
