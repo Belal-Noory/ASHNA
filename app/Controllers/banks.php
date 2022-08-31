ab73d305f6a028850a89d379dd1c80247c8f3153
Belal Noory <belalnoory2@gmail.com>
2022-08-31 04:01:39 -0700

    updated

--- app/Controllers/banks.php
+++ app/Controllers/banks.php
@@ -220,8 +220,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // get currency details
         $currency_data = json_decode($company->GetCurrencyByName($account_currency->currency, $loged_user->company_id));
         
-        // $res = $banks->addOpeningBalanceLeadger([$account, $currency_data->company_currency_id, "Opening Balance", $financial_term, time(), 1, $loged_user->user_id, 0, "Opening Balance", $loged_user->company_id]);
-        $res = $banks->addTransferMoney([$account, 12, $amoun, "Debet", $loged_user->company_id, "Opening Balance",0]);
+        $res = $banks->addOpeningBalanceLeadger([$account, $currency_data->company_currency_id, "Opening Balance", $financial_term, time(), 1, $loged_user->user_id, 0, "Opening Balance", $loged_user->company_id]);
+        $banks->addTransferMoney([$account, 12, $amoun, "Debet", $loged_user->company_id, "Opening Balance",0]);
         // if more data submitted
         $count = $_POST["rowCount"];
         if($count > 1)
