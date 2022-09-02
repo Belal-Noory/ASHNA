855c7df8059e2a19fc5efa5eb42e3932f3796ebf
Belal Noory <belalnoory2@gmail.com>
2022-09-02 09:38:26 -0700

    updated

--- business/NewExpense.php
+++ business/NewExpense.php
@@ -389,14 +389,14 @@ include("./master/footer.php");
 
 <script>
     $(document).ready(function() {
-        var seen = {};
-        $('option').each(function() {
-            var txt = $(this).text();
-            if (seen[txt])
-                $(this).remove();
-            else
-                seen[txt] = true;
-        });
+        // var seen = {};
+        // $('option').each(function() {
+        //     var txt = $(this).text();
+        //     if (seen[txt])
+        //         $(this).remove();
+        //     else
+        //         seen[txt] = true;
+        // });
         // Add recept
         $("#btnaddreceipt").on("click", function() {
             if ($(".form").valid()) {
