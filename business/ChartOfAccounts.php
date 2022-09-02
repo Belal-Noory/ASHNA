eea6a88c23b28321ef5e8061cff331556d609410
Belal Noory <belalnoory2@gmail.com>
2022-09-02 11:45:45 -0700

    updated

--- business/ChartOfAccounts.php
+++ business/ChartOfAccounts.php
@@ -18,8 +18,8 @@ function recurSearch2($c, $parentID)
     $conn = new Connection();
     $query = "SELECT * FROM account_catagory 
     LEFT JOIN chartofaccount ON account_catagory.account_catagory_id = chartofaccount.account_catagory 
-    WHERE parentID = ? AND chartofaccount.company_id = ? AND chartofaccount.useradded = ?";
-    $result = $conn->Query($query, [$parentID, $c, 0]);
+    WHERE parentID = ? AND chartofaccount.company_id = ?";
+    $result = $conn->Query($query, [$parentID, $c]);
     $results = $result->fetchAll(PDO::FETCH_OBJ);
     foreach ($results as $item) {
         if (checkChilds($item->account_catagory_id) > 0) {
@@ -35,8 +35,8 @@ function recurSearch2($c, $parentID)
 function checkChilds($patne)
 {
     $conn = new Connection();
-    $query = "SELECT * FROM account_catagory WHERE parentID = ? AND useradded = ?";
-    $result = $conn->Query($query, [$patne, 0]);
+    $query = "SELECT * FROM account_catagory WHERE parentID = ?";
+    $result = $conn->Query($query, [$patne]);
     $results = $result->rowCount();
     return $results;
 }
