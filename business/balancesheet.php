969e0242cf48610e4191cbdc5dc847c46cf752a0
Belal Noory <belalnoory2@gmail.com>
2022-09-02 00:54:50 -0700

    updated

--- business/balancesheet.php
+++ business/balancesheet.php
@@ -50,11 +50,14 @@ function recurSearch2($c, $parentID)
                 }
             }
         }
-        echo "<a href='#' class='list-group-item list-group-item-action balancehover d-flex justify-content-evenly' id='$item->chartofaccount_id' style='background-color: transparent;color:rgba(0,0,0,.5);' aria-current='true'>
-                                    <span style='margin-right:auto'>$item->account_name</span>
-                                    <span class='total'>$total</span>
-                                </a>";
-        $total = 0;
+        if($item->useradded === "0" || $item->useradded === 0)
+        {
+            echo "<a href='#' class='list-group-item list-group-item-action balancehover d-flex justify-content-evenly' id='$item->chartofaccount_id' style='background-color: transparent;color:rgba(0,0,0,.5);' aria-current='true'>
+                                        <span style='margin-right:auto'>$item->account_name</span>
+                                        <span class='total'>$total</span>
+                                    </a>";
+            $total = 0;
+        }
         if (checkChilds($item->account_catagory_id) > 0) {
             recurSearch2($c, $item->account_catagory_id);
         }
