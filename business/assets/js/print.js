09508a0f972c896e9506584f4e3490aea6fc2d3c
Belal Noory <belalnoory2@gmail.com>
2022-09-10 23:13:04 -0700

    updated

--- business/assets/js/print.js
+++ business/assets/js/print.js
@@ -3,30 +3,28 @@ function print(data) {
     var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
     path = baseUrl + '/bussiness/assets/css/print.css';
     $("#printimg").attr("src", baseUrl + "/business/app-assets/images/logo/ashna_trans.png");
-    if ($(".customer").length > 0) {
-        $("#printtitle").text($(document).attr('title'));
-        $("#pdate").text(data.date);
+    $("#printtitle").text($(document).attr('title'));
+    $("#pdate").text(data.date);
 
-        // get current time
-        var now = new Date(Date.now());
-        var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-        $("#ptime").text(formatted);
+    // get current time
+    var now = new Date(Date.now());
+    var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    $("#ptime").text(formatted);
 
-        $("#plid").text(data.lid);
-        $("#prid").text(data.tid);
-        $("#pcurrency").text(data.currency);
-        $("#pamount").text(data.amount);
+    $("#plid").text(data.lid);
+    $("#prid").text(data.tid);
+    $("#pcurrency").text(data.currency);
+    $("#pamount").text(data.amount);
 
-        $("#rfrom").text(data.from);
-        $("#pdetiails").text(data.details);
-        $("#wordamount").text(toWords(data.amount));
+    $("#rfrom").text(data.from);
+    $("#pdetiails").text(data.details);
+    $("#wordamount").text(toWords(data.amount));
 
-        $("#pby").text(data.pby);
-        // $("#vby").text(data.vby);
+    $("#pby").text(data.pby);
+    // $("#vby").text(data.vby);
 
-        $(".print").printThis({
-            importCSS: false,
-            importStyle: true,
-        });
-    }
+    $(".print").printThis({
+        importCSS: false,
+        importStyle: true,
+    });
 }
