8102088561c56da1b6fda9ceb90a1ad7581c2c61
Belal Noory <belalnoory2@gmail.com>
2022-09-11 00:41:00 -0700

    updated

--- app/Controllers/Transfer.php
+++ app/Controllers/Transfer.php
@@ -157,7 +157,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $cdetails_data = $company->GetCurrencyDetails($currency);
         $cdetails = $cdetails_data->fetch(PDO::FETCH_OBJ);
 
-        $ret = array('date' => $date, 'lid' => $leadger_id, 'tid' => $transfer_ID, $tid, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname,'tcode'=>$transfercode,'address'=>$saraf_cus_id_details->office_address+","+$saraf_cus_id_details->official_phone+","+$saraf_cus_id_details->personal_phone);
+        $ret = array('date' => $date, 'lid' => $leadger_id, 'tid' => $transfer_ID, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname,'tcode'=>$transfercode,'address'=>$saraf_cus_id_details->office_address+","+$saraf_cus_id_details->official_phone+","+$saraf_cus_id_details->personal_phone);
         echo json_encode($ret);
     }
 
@@ -267,7 +267,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $cdetails_data = $company->GetCurrencyDetails($currency);
         $cdetails = $cdetails_data->fetch(PDO::FETCH_OBJ);
 
-        $ret = array('date' => $date, 'lid' => $leadger_id, 'tid' => $transfer_ID, $tid, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname,'tcode'=>$transfercode,'address'=>$saraf_cus_id_details->office_address+","+$saraf_cus_id_details->official_phone+","+$saraf_cus_id_details->personal_phone);
+        $ret = array('date' => $date, 'lid' => $leadger_id, 'tid' => $transfer_ID, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname,'tcode'=>$transfercode,'address'=>$saraf_cus_id_details->office_address+","+$saraf_cus_id_details->official_phone+","+$saraf_cus_id_details->personal_phone);
         echo json_encode($ret);
     }
 
