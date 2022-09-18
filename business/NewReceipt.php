42acef58b938f788d022d68fda9af91e910e2395
Belal Noory <belalnoory2@gmail.com>
2022-09-18 01:45:42 -0700

    updtaed

--- business/NewReceipt.php
+++ business/NewReceipt.php
@@ -209,6 +209,8 @@ $allContacts = $allContacts_data->fetchAll(PDO::FETCH_OBJ);
                 $receipts = $receipts_data->fetch(PDO::FETCH_OBJ);
                 $account_details = $banks->getBankByID($receipts->recievable_id);
                 $account = $account_details->fetch(PDO::FETCH_OBJ);
+
+                print_r($account);
             ?>
                 <div class="col-md-12">
                     <div class="card">
