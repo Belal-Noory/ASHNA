a8c68cdf608b135df2f2875675e2733ba689789b
Belal Noory <belalnoory2@gmail.com>
2022-09-26 03:12:03 -0700

    updated

--- business/Banks.php
+++ business/Banks.php
@@ -62,81 +62,67 @@ $colors = array("info", "danger", "success", "warning");
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body p-2">
-                <div class="card">
-                    <div class="card-header">
-                        <a class="heading-elements-toggle"><i class="la la-ellipsis-h font-medium-3"></i></a>
-                        <div class="heading-elements">
-                            <ul class="list-inline mb-0">
-                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                            </ul>
+                <form action="#" class="form">
+                    <h4 class="form-section"><i class="ft-user"></i> Bank Info</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="account_name" style="font-variant:small-caps">
+                                    account name:
+                                    <span class="danger">*</span>
+                                </label>
+                                <input type="text" class="form-control required" id="account_name" name="account_name" placeholder="ACCOUNT NAME">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="account_number" style="font-variant:small-caps">
+                                    account number:
+                                    <span class="danger">*</span>
+                                </label>
+                                <input type="text" class="form-control required" id="account_number" name="account_number" placeholder="ACCOUNT NUMBER">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="initial_ammount" style="font-variant:small-caps">
+                                    initial ammount:
+                                    <span class="danger">*</span>
+                                </label>
+                                <input type="text" class="form-control required" id="initial_ammount" name="initial_ammount" placeholder="INITIAL AMMOUNT">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="currency" style="font-variant:small-caps">
+                                    currency:
+                                    <span class="danger">*</span>
+                                </label>
+                                <select id="currency" name="currency" class="form-control">
+                                    <?php
+                                    foreach ($allcurrency as $currency) {
+                                        echo "<option value='$currency->company_currency_id'>$currency->currency</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label for="note" style="font-variant:small-caps">
+                                    note:
+                                    <span class="danger">*</span>
+                                </label>
+                                <textarea class="form-control" id="note" name="note" placeholder="NOTE"></textarea>
+                            </div>
                         </div>
                     </div>
-                    <div class="card-content collapse show">
-                        <div class="card-body">
-                            <form action="#" class="form">
-                                <h4 class="form-section"><i class="ft-user"></i> Bank Info</h4>
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label for="account_name" style="font-variant:small-caps">
-                                                account name:
-                                                <span class="danger">*</span>
-                                            </label>
-                                            <input type="text" class="form-control required" id="account_name" name="account_name" placeholder="ACCOUNT NAME">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label for="account_number" style="font-variant:small-caps">
-                                                account number:
-                                                <span class="danger">*</span>
-                                            </label>
-                                            <input type="text" class="form-control required" id="account_number" name="account_number" placeholder="ACCOUNT NUMBER">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label for="initial_ammount" style="font-variant:small-caps">
-                                                initial ammount:
-                                                <span class="danger">*</span>
-                                            </label>
-                                            <input type="text" class="form-control required" id="initial_ammount" name="initial_ammount" placeholder="INITIAL AMMOUNT">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label for="currency" style="font-variant:small-caps">
-                                                currency:
-                                                <span class="danger">*</span>
-                                            </label>
-                                            <select id="currency" name="currency" class="form-control">
-                                                <?php
-                                                foreach ($allcurrency as $currency) {
-                                                    echo "<option value='$currency->company_currency_id'>$currency->currency</option>";
-                                                }
-                                                ?>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <div class="form-group">
-                                            <label for="note" style="font-variant:small-caps">
-                                                note:
-                                                <span class="danger">*</span>
-                                            </label>
-                                            <textarea class="form-control" id="note" name="note" placeholder="NOTE"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-actions">
-                                    <button type="submite" class="btn btn-blue waves-effect waves-light">
-                                        <i class="la la-check-square-o"></i> Update
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                    <div class="form-actions">
+                        <button type="submite" class="btn btn-blue waves-effect waves-light">
+                            <i class="la la-check-square-o"></i> Update
+                        </button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
@@ -148,13 +134,16 @@ include("./master/footer.php");
 ?>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
 
         // load bank
-        $(document).on("click",".btneshowedite",function(e){
+        $(document).on("click", ".btneshowedite", function(e) {
             e.preventDefault();
             bID = $(this).attr("data-href");
-            $.get("../app/Controllers/banks.php",{accountDetails:true,acc:bID},function(data){
+            $.get("../app/Controllers/banks.php", {
+                accountDetails: true,
+                acc: bID
+            }, function(data) {
                 ndata = $.parseJSON(data);
                 console.log(data);
 
