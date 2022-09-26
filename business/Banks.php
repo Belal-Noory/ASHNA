32e92da7c6833c308696158e98104a27c6f6e876
Belal Noory <belalnoory2@gmail.com>
2022-09-26 03:12:51 -0700

    updated

--- business/Banks.php
+++ business/Banks.php
@@ -58,7 +58,7 @@ $colors = array("info", "danger", "success", "warning");
 </section>
 
 <!-- Add Reminder Modal -->
-<div class="modal fade text-center" id="showeditbankModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel5" aria-hidden="true">
+<div class="modal fade" id="showeditbankModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel5" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body p-2">
