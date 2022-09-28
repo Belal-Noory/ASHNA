548a4c74339e66284e8f92fd67d109a802d123a9
Belal Noory <belalnoory2@gmail.com>
2022-09-28 01:59:56 -0700

    updated

--- business/profitloss.php
+++ business/profitloss.php
@@ -327,15 +327,15 @@ function recurSearchCapital($c, $parentID, $amount_type, $catanme)
                                 foreach ($RES as $LID) {
                                     if ($LID->ammount_type == "Crediet") {
                                         if ($LID->rate != 0) {
-                                            $total -= ($LID->amount * $LID->rate);
-                                        } else {
-                                            $total -= $LID->amount;
-                                        }
-                                    } else {
-                                        if ($LID->rate != 0) {
                                             $total += ($LID->amount * $LID->rate);
                                         } else {
                                             $total += $LID->amount;
+                                        }
+                                    } else {
+                                        if ($LID->rate != 0) {
+                                            $total -= ($LID->amount * $LID->rate);
+                                        } else {
+                                            $total -= $LID->amount;
                                         }
                                     }
                                 }
