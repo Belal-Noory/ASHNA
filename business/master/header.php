2e62f9ea42924efd227b7d13c107beecfa256e79
Belal Noory <belalnoory2@gmail.com>
2022-10-19 22:45:51 -0700

    updated

--- business/master/header.php
+++ business/master/header.php
@@ -122,6 +122,8 @@ $notifications_count_data = $admin->getPendingTransactionsCount($user_data->comp
     <meta name="author" content="Belal Noory">
     <title><?php echo $page_title; ?></title>
     <link rel="icon" href="../ashna.ico" />
+    <link rel="apple-touch-icon" href="app-assets/images/logo/ashna.png">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/logo/ashna.png">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
