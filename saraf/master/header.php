22b0b2698caa7eafe1631094a1884c228c6b8a61
Belal Noory <belalnoory2@gmail.com>
2022-10-19 22:23:14 -0700

    updated

--- saraf/master/header.php
+++ saraf/master/header.php
@@ -14,13 +14,11 @@ $loged_user = json_decode($_SESSION["saraf_user"]);
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
-    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
-    <meta name="author" content="PIXINVENT">
+    <meta name="description" content="ASHNA BABUR accounting software, expections in accounting">
+    <meta name="keywords" content="ashna babur, ashna, ashna babur accounting system, accounting software, msp services, ashna msp">
+    <meta name="author" content="Belal Noory">
     <title><?php echo $page_title; ?></title>
-    <link rel="apple-touch-icon" href="../business/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../business/app-assets/images/ico/favicon.ico">
+    <link rel="icon" href="../ashna.ico" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
