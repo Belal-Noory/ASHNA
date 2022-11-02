1d767661f92b854a14c189ad4827d91b44d46f6e
Belal Noory <belalnoory2@gmail.com>
2022-11-02 02:01:06 -0700

    updated

--- app/Controllers/Transfer.php
+++ app/Controllers/Transfer.php
@@ -60,140 +60,140 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         // Daily Customer sender 
-        $Daily_sender_id = 0;
-        if ($_POST["addsender"] == "true") {
-            $sender_phone = helper::test_input($_POST["sender_phone"]);
-            $sender_fname = helper::test_input($_POST["sender_fname"]);
-            $sender_lname = helper::test_input($_POST["sender_lname"]);
-            $sender_Fathername = helper::test_input($_POST["sender_Fathername"]);
-            $sender_nid = helper::test_input($_POST["sender_nid"]);
-            $sender_details = helper::test_input($_POST["sender_details"]);
-            $Daily_sender_id = $bussiness->addDailyCustomer([$sender_fname, $sender_lname, $sender_Fathername, $sender_phone, $sender_nid, $sender_details, 'Daily Customer', time(), $loged_user->user_id, 1]);
-            $fileNAmeTmp = time() . $_FILES["attachmentsender"]['name'];
-            if (move_uploaded_file($_FILES['attachmentsender']['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNAmeTmp)) {
-                $type = $_POST["attachTypesender"];
-                $bussiness->addDailyCustomerAttachment([$Daily_sender_id, $fileNAmeTmp, $type]);
-            }
-            $totalsenderAttachemnts = $_POST["attachCountsender"];
-            if ($totalsenderAttachemnts > 0) {
-                for ($i = 1; $i <= $totalsenderAttachemnts; $i++) {
-                    $fileNameTmp = time() . $_FILES[('attachmentsender' . $i)]['name'];
-                    if (move_uploaded_file($_FILES[('attachmentsender' . $i)]['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNameTmp)) {
-                        $type_tmp = $_POST[("attachTypesender" . $i)];
-                        $bussiness->addDailyCustomerAttachment([$Daily_sender_id, $fileNameTmp, $type_tmp]);
-                    }
-                }
-            }
-        } else {
-            $daily_sender_data = $bussiness->GetDailyCustomer(helper::test_input($_POST["sender_phone"]));
-            $daily_sender_details = $daily_sender_data->fetch(PDO::FETCH_OBJ);
-            $Daily_sender_id = $daily_sender_details->customer_id;
-        }
+        // $Daily_sender_id = 0;
+        // if ($_POST["addsender"] == "true") {
+        //     $sender_phone = helper::test_input($_POST["sender_phone"]);
+        //     $sender_fname = helper::test_input($_POST["sender_fname"]);
+        //     $sender_lname = helper::test_input($_POST["sender_lname"]);
+        //     $sender_Fathername = helper::test_input($_POST["sender_Fathername"]);
+        //     $sender_nid = helper::test_input($_POST["sender_nid"]);
+        //     $sender_details = helper::test_input($_POST["sender_details"]);
+        //     $Daily_sender_id = $bussiness->addDailyCustomer([$sender_fname, $sender_lname, $sender_Fathername, $sender_phone, $sender_nid, $sender_details, 'Daily Customer', time(), $loged_user->user_id, 1]);
+        //     $fileNAmeTmp = time() . $_FILES["attachmentsender"]['name'];
+        //     if (move_uploaded_file($_FILES['attachmentsender']['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNAmeTmp)) {
+        //         $type = $_POST["attachTypesender"];
+        //         $bussiness->addDailyCustomerAttachment([$Daily_sender_id, $fileNAmeTmp, $type]);
+        //     }
+        //     $totalsenderAttachemnts = $_POST["attachCountsender"];
+        //     if ($totalsenderAttachemnts > 0) {
+        //         for ($i = 1; $i <= $totalsenderAttachemnts; $i++) {
+        //             $fileNameTmp = time() . $_FILES[('attachmentsender' . $i)]['name'];
+        //             if (move_uploaded_file($_FILES[('attachmentsender' . $i)]['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNameTmp)) {
+        //                 $type_tmp = $_POST[("attachTypesender" . $i)];
+        //                 $bussiness->addDailyCustomerAttachment([$Daily_sender_id, $fileNameTmp, $type_tmp]);
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     $daily_sender_data = $bussiness->GetDailyCustomer(helper::test_input($_POST["sender_phone"]));
+        //     $daily_sender_details = $daily_sender_data->fetch(PDO::FETCH_OBJ);
+        //     $Daily_sender_id = $daily_sender_details->customer_id;
+        // }
 
-        // Daily Customer receiver 
-        $Daily_receiver_id = 0;
-        if ($_POST["addreceiver"] == "true") {
-            $receiver_phone = helper::test_input($_POST["receiver_phone"]);
-            $receiver_fname = helper::test_input($_POST["receiver_fname"]);
-            $receiver_lname = helper::test_input($_POST["receiver_lname"]);
-            $receiver_Fathername = helper::test_input($_POST["receiver_Fathername"]);
-            $receiver_nid = helper::test_input($_POST["receiver_nid"]);
-            $receiver_details = helper::test_input($_POST["receiver_details"]);
-            $Daily_receiver_id = $bussiness->addDailyCustomer([$receiver_fname, $receiver_lname, $receiver_Fathername, $receiver_phone, $receiver_nid, $receiver_details, 'Daily Customer', time(), $loged_user->user_id, 1]);
+        // // Daily Customer receiver 
+        // $Daily_receiver_id = 0;
+        // if ($_POST["addreceiver"] == "true") {
+        //     $receiver_phone = helper::test_input($_POST["receiver_phone"]);
+        //     $receiver_fname = helper::test_input($_POST["receiver_fname"]);
+        //     $receiver_lname = helper::test_input($_POST["receiver_lname"]);
+        //     $receiver_Fathername = helper::test_input($_POST["receiver_Fathername"]);
+        //     $receiver_nid = helper::test_input($_POST["receiver_nid"]);
+        //     $receiver_details = helper::test_input($_POST["receiver_details"]);
+        //     $Daily_receiver_id = $bussiness->addDailyCustomer([$receiver_fname, $receiver_lname, $receiver_Fathername, $receiver_phone, $receiver_nid, $receiver_details, 'Daily Customer', time(), $loged_user->user_id, 1]);
 
-            $fileNAmeTmp = time() . $_FILES["attachmentreceiver"]['name'];
-            if (move_uploaded_file($_FILES['attachmentreceiver']['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNAmeTmp)) {
-                $type_r = $_POST["attachTypereceiver"];
-                $bussiness->addDailyCustomerAttachment([$Daily_receiver_id, $fileNAmeTmp, $type_r]);
-            }
+        //     $fileNAmeTmp = time() . $_FILES["attachmentreceiver"]['name'];
+        //     if (move_uploaded_file($_FILES['attachmentreceiver']['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNAmeTmp)) {
+        //         $type_r = $_POST["attachTypereceiver"];
+        //         $bussiness->addDailyCustomerAttachment([$Daily_receiver_id, $fileNAmeTmp, $type_r]);
+        //     }
 
-            $totalreceiverAttachemnts = $_POST["attachCountreceiver"];
-            if ($totalreceiverAttachemnts > 0) {
-                for ($i = 1; $i <= $totalreceiverAttachemnts; $i++) {
+        //     $totalreceiverAttachemnts = $_POST["attachCountreceiver"];
+        //     if ($totalreceiverAttachemnts > 0) {
+        //         for ($i = 1; $i <= $totalreceiverAttachemnts; $i++) {
 
-                    $fileNameTmp = time() . $_FILES[('attachmentsender' . $i)]['name'];
-                    if (move_uploaded_file($_FILES[('attachmentsender' . $i)]['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNameTmp)) {
-                        $type_r_tmp = $_POST[("attachTypereceiver" . $i)];
-                        $bussiness->addDailyCustomerAttachment([$Daily_receiver_id, $fileNameTmp]);
-                    }
-                }
-            }
-        } else {
-            $daily_receiver_data = $bussiness->GetDailyCustomer(helper::test_input($_POST["sender_phone"]));
-            $daily_receiver_details = $daily_receiver_data->fetch(PDO::FETCH_OBJ);
-            $Daily_receiver_id = $daily_receiver_details->customer_id;
-        }
+        //             $fileNameTmp = time() . $_FILES[('attachmentsender' . $i)]['name'];
+        //             if (move_uploaded_file($_FILES[('attachmentsender' . $i)]['tmp_name'], "../../business/uploadedfiles/dailycustomer/" . $fileNameTmp)) {
+        //                 $type_r_tmp = $_POST[("attachTypereceiver" . $i)];
+        //                 $bussiness->addDailyCustomerAttachment([$Daily_receiver_id, $fileNameTmp]);
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     $daily_receiver_data = $bussiness->GetDailyCustomer(helper::test_input($_POST["sender_phone"]));
+        //     $daily_receiver_details = $daily_receiver_data->fetch(PDO::FETCH_OBJ);
+        //     $Daily_receiver_id = $daily_receiver_details->customer_id;
+        // }
 
-        // just add one payment method
-        $paymentID = $_POST["reciptItemID"];
-        $payment_amount = $_POST["reciptItemAmount"] - ($sarafcommission + $mycommission);
-        $company_financial_term_id = 0;
-        if (isset($company_ft->term_id)) {
-            $company_financial_term_id = $company_ft->term_id;
-        }
-        $recipt_details = helper::test_input($_POST["reciptItemdetails"]);
+        // // just add one payment method
+        // $paymentID = $_POST["reciptItemID"];
+        // $payment_amount = $_POST["reciptItemAmount"] - ($sarafcommission + $mycommission);
+        // $company_financial_term_id = 0;
+        // if (isset($company_ft->term_id)) {
+        //     $company_financial_term_id = $company_ft->term_id;
+        // }
+        // $recipt_details = helper::test_input($_POST["reciptItemdetails"]);
 
-        // get currency details
-        $currency_data = $company->GetCompanyCurrencyDetails($loged_user->company_id, $currency);
-        $currency_details = $currency_data->fetch(PDO::FETCH_OBJ);
+        // // get currency details
+        // $currency_data = $company->GetCompanyCurrencyDetails($loged_user->company_id, $currency);
+        // $currency_details = $currency_data->fetch(PDO::FETCH_OBJ);
 
-        // get the rate of the currency
-        $rate = 0;
-        if ($currency_details->currency !== $mainCurency) {
-            $currency_rate_details_data = $bank->getExchangeConversion($mainCurency, $currency_details->currency, $loged_user->company_id);
-            $currency_rate_details = $currency_rate_details_data->fetch(PDO::FETCH_OBJ);
-            if ($currency_rate_details->currency_from == $mainCurency) {
-                $rate = 1 / $currency_rate_details->rate;
-            } else {
-                $rate = $currency_rate_details->rate;
-            }
-        }
+        // // get the rate of the currency
+        // $rate = 0;
+        // if ($currency_details->currency !== $mainCurency) {
+        //     $currency_rate_details_data = $bank->getExchangeConversion($mainCurency, $currency_details->currency, $loged_user->company_id);
+        //     $currency_rate_details = $currency_rate_details_data->fetch(PDO::FETCH_OBJ);
+        //     if ($currency_rate_details->currency_from == $mainCurency) {
+        //         $rate = 1 / $currency_rate_details->rate;
+        //     } else {
+        //         $rate = $currency_rate_details->rate;
+        //     }
+        // }
 
-        // Get Last Leadger ID of company
-        $LastLID = $company->getLeadgerID($loged_user->company_id, "transferout");
-        $LastLID = "TOU-" . $LastLID;
+        // // Get Last Leadger ID of company
+        // $LastLID = $company->getLeadgerID($loged_user->company_id, "transferout");
+        // $LastLID = "TOU-" . $LastLID;
 
-        $transfer->addTransferOutLeadger([$LastLID, $paymentID, $rsaraf_ID, $company_financial_term_id, $newdate, $details, 0, $loged_user->user_id, 0, "transferout", $loged_user->company_id, $currency]);
-        // Credit the required amount in Saraf`s account
-        $transfer->addTransferOutMoney([$rsaraf_ID, $LastLID, $amount, "Crediet", $loged_user->company_id, $recipt_details, 1, $currency, $rate]);
-        if($sarafcommission > 0)
-        {
-            $transfer->addTransferOutMoney([$rsaraf_ID, $LastLID, $sarafcommission, "Crediet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
-        }
+        // $transfer->addTransferOutLeadger([$LastLID, $paymentID, $rsaraf_ID, $company_financial_term_id, $newdate, $details, 0, $loged_user->user_id, 0, "transferout", $loged_user->company_id, $currency]);
+        // // Credit the required amount in Saraf`s account
+        // $transfer->addTransferOutMoney([$rsaraf_ID, $LastLID, $amount, "Crediet", $loged_user->company_id, $recipt_details, 1, $currency, $rate]);
+        // if($sarafcommission > 0)
+        // {
+        //     $transfer->addTransferOutMoney([$rsaraf_ID, $LastLID, $sarafcommission, "Crediet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
+        // }
 
-        // Debit the required amount from Company Account
-        $transfer->addTransferOutMoney([$paymentID, $LastLID, $payment_amount, "Debet", $loged_user->company_id, $details, 1, $currency, $rate]);
-        if($mycommission > 0)
-        {
-            $transfer->addTransferOutMoney([$paymentID, $LastLID, $mycommission, "Debet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
-        }
-        if($sarafcommission > 0)
-        {
-            $transfer->addTransferOutMoney([$paymentID, $LastLID, $sarafcommission, "Debet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
-        }
-        // Add the transfer profit to Income out transfer account in chartofaccount
-        $incomeTransfer_data = $bank->getAccountByName($loged_user->company_id, "Income out Transfer");
-        $incomeTransfer = $incomeTransfer_data->fetch(PDO::FETCH_OBJ);
-        if($mycommission > 0)
-        {
-            $transfer->addTransferOutMoney([$incomeTransfer->chartofaccount_id, $LastLID, $mycommission, "Crediet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
-        }
+        // // Debit the required amount from Company Account
+        // $transfer->addTransferOutMoney([$paymentID, $LastLID, $payment_amount, "Debet", $loged_user->company_id, $details, 1, $currency, $rate]);
+        // if($mycommission > 0)
+        // {
+        //     $transfer->addTransferOutMoney([$paymentID, $LastLID, $mycommission, "Debet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
+        // }
+        // if($sarafcommission > 0)
+        // {
+        //     $transfer->addTransferOutMoney([$paymentID, $LastLID, $sarafcommission, "Debet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
+        // }
+        // // Add the transfer profit to Income out transfer account in chartofaccount
+        // $incomeTransfer_data = $bank->getAccountByName($loged_user->company_id, "Income out Transfer");
+        // $incomeTransfer = $incomeTransfer_data->fetch(PDO::FETCH_OBJ);
+        // if($mycommission > 0)
+        // {
+        //     $transfer->addTransferOutMoney([$incomeTransfer->chartofaccount_id, $LastLID, $mycommission, "Crediet", $loged_user->company_id, "Commission", 1, $currency, $rate]);
+        // }
 
-        $saraf_cus_id_data = $bank->getCustomerByBank($rsaraf_ID);
-        $saraf_cus_id_details = $saraf_cus_id_data->fetch(PDO::FETCH_OBJ);
+        // $saraf_cus_id_data = $bank->getCustomerByBank($rsaraf_ID);
+        // $saraf_cus_id_details = $saraf_cus_id_data->fetch(PDO::FETCH_OBJ);
 
-        $transfer_ID = $transfer->addOutTransfer([$loged_user->customer_id, $mycommission, $saraf_cus_id_details->customer_id, $sarafcommission, $Daily_sender_id, $Daily_receiver_id, $amount, $currency, $newdate, 0, 0, $transfercode, $vouchercode, $details, 0, "out", $loged_user->company_id, $LastLID, $financial_term]);
+        // $transfer_ID = $transfer->addOutTransfer([$loged_user->customer_id, $mycommission, $saraf_cus_id_details->customer_id, $sarafcommission, $Daily_sender_id, $Daily_receiver_id, $amount, $currency, $newdate, 0, 0, $transfercode, $vouchercode, $details, 0, "out", $loged_user->company_id, $LastLID, $financial_term]);
 
-        // get currency details
-        $cdetails_data = $company->GetCurrencyDetails($currency);
-        $cdetails = $cdetails_data->fetch(PDO::FETCH_OBJ);
+        // // get currency details
+        // $cdetails_data = $company->GetCurrencyDetails($currency);
+        // $cdetails = $cdetails_data->fetch(PDO::FETCH_OBJ);
 
-        $address = "";
-        if (isset($saraf_cus_id_details->office_address) || isset($saraf_cus_id_details->official_phone) || $saraf_cus_id_details->personal_phone) {
-            $address = $saraf_cus_id_details->office_address . "," . $saraf_cus_id_details->official_phone . "," . $saraf_cus_id_details->personal_phone;
-        }
-        $ret = array('date' => $date, 'lid' => $LastLID, 'tid' => $transfer_ID, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname, 'tcode' => $transfercode, 'address' => $address, 'from' => $saraf_cus_id_details->alies_name);
-        echo json_encode($ret);
+        // $address = "";
+        // if (isset($saraf_cus_id_details->office_address) || isset($saraf_cus_id_details->official_phone) || $saraf_cus_id_details->personal_phone) {
+        //     $address = $saraf_cus_id_details->office_address . "," . $saraf_cus_id_details->official_phone . "," . $saraf_cus_id_details->personal_phone;
+        // }
+        // $ret = array('date' => $date, 'lid' => $LastLID, 'tid' => $transfer_ID, 'currency' => $cdetails->currency, 'amount' => $amount, 'details' => $details, 'pby' => $loged_user->fname . ' ' . $loged_user->lname, 'tcode' => $transfercode, 'address' => $address, 'from' => $saraf_cus_id_details->alies_name);
+        echo $transfercode;
     }
 
 
