8db2ff2653f3a49ac3d25fe7cc9875633e67923d
Belal Noory <belalnoory2@gmail.com>
2022-11-08 00:04:11 -0800

    udpated

--- app/Models/Banks.php
+++ app/Models/Banks.php
@@ -152,6 +152,7 @@ class Banks
     {
         $query = "SELECT * FROM account_money 
         INNER JOIN general_leadger ON general_leadger.leadger_id = account_money.leadger_ID 
+        INNER JOIN company_currency ON account_money.currency = company_currency.company_currency_id 
         WHERE account_id = ? AND general_leadger.company_financial_term_id = ? 
         AND account_money.temp = ? AND general_leadger.cleared = ?";
         $result = $this->conn->Query($query, [$ID,$termid,0,0]);
