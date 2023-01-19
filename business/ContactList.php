0cd438553013b9301f43989b11c4a9e5972dba1b
Belal Noory <belalnoory2@gmail.com>
2023-01-19 14:53:46 +0430

    updated

--- business/ContactList.php
+++ business/ContactList.php
@@ -147,7 +147,7 @@ if (isset($company_ft->term_id)) {
                                         }
                                     }
                                     $totalRecevible = ($debit - $credit);
-                                    $Balance = ($totalPayable-$totalRecevible);
+                                    $Balance = ($totalRecevible-$totalPayable);
                                 ?>
                                     <tr>
                                         <td><a href="#" data-href="<?php echo $customer->customer_id; ?>" class="showcustomerdetails"><?php echo $customer->alies_name; ?></a></td>
