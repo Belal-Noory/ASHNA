75a7b62259173d2a06e031725c32a7a7e29e1560
Belal Noory <belalnoory2@gmail.com>
2023-01-19 13:24:13 +0430

    test

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -21,9 +21,12 @@ class Receipt
     public function getReceiptLeadger($companyID, $term_id)
     {
         $query = "SELECT * FROM general_leadger 
+        LEFT JOIN account_money ON general_leadger.leadger_id = account_money.leadger_ID 
+        LEFT JOIN company_currency ON general_leadger.currency_id = company_currency.company_currency_id 
         WHERE general_leadger.company_id = ? AND general_leadger.op_type = ? AND general_leadger.cleared=? 
-        AND general_leadger.company_financial_term_id = ?  AND general_leadger.approved = ? AND deleted = ?";
-        $result = $this->conn->Query($query, [$companyID, "Receipt", 0, $term_id, 1, 0]);
+        AND general_leadger.company_financial_term_id = ? AND account_money.ammount_type = ? 
+        AND general_leadger.approved = ? AND deleted = ?";
+        $result = $this->conn->Query($query, [$companyID, "Receipt", 0, $term_id, "Crediet", 1, 0]);
         return $result;
     }
 
