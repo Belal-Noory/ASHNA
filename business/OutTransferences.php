acdf76821d1af78a43be7146b567711a204d112c
Belal Noory <belalnoory2@gmail.com>
2023-02-21 12:58:20 +0430

    updated

--- business/OutTransferences.php
+++ business/OutTransferences.php
@@ -155,10 +155,10 @@ $paid_transfers = $paid_transfers_data->fetchAll(PDO::FETCH_OBJ);
                                                         </thead>
                                                         <tbody>
                                                             <?php
+                                                            print_r($pending_transfers);
                                                             foreach ($pending_transfers as $ptransfer) {
                                                                 $sender_data = $bussiness->getCustomerByID($ptransfer->money_sender);
                                                                 $sender = $sender_data->fetch(PDO::FETCH_OBJ);
-                                                                echo $ptransfer->money_sender."||||".$ptransfer->money_receiver;
                                                                 $receiver_data = $bussiness->getCustomerByID($ptransfer->money_receiver);
                                                                 $receiver = $receiver_data->fetch(PDO::FETCH_OBJ);
 
